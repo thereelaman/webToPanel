5988ecdbe3b7195a3de9e4e6053dbcda831cabb1
thereelaman <amandwivedi@mail.com>
2020-10-27 16:44:17 +0530

    replaced custom pull with vincenteguerra/git-deploy

---
+++ api/deploy/deploy.php
@@ -0,0 +1,12 @@
+<?php
+define("TOKEN", "");                                                        // The secret token.
+define("REMOTE_REPOSITORY", "git@github.com:thereelaman/webToPanel.git");   // The SSH URL to your repository
+define("DIR", "/var/www/thedisplay.studio/");                               // The path to your repostiroy; this must begin with a forward slash (/)
+define("BRANCH", "refs/heads/master");                                      // The branch route
+define("LOGFILE", "deploy.log");                                            // The name of the file you want to log to.
+define("GIT", "/usr/bin/git");                                              // The path to the git executable
+define("MAX_EXECUTION_TIME", 180);                                          // Override for PHP's max_execution_time (may need set in php.ini)
+define("BEFORE_PULL", "cd /var/www/thedisplay.studio/");                    // A command to execute before pulling
+define("AFTER_PULL", "git reset –hard HEAD && git pull");                   // A command to execute after successfully pulling
+
+require_once("deployer.php");
--- api/pull.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ( $_POST['payload'] ) {
-shell_exec( ‘cd /var/www/thedisplay.studio/ && git reset –hard HEAD && git pull’ );
-}
-
-?>hi
