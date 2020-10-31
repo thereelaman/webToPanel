38996a42049d5da6573d37a38ab073898477316f
thereelaman <amandwivedi@mail.com>
2020-10-31 16:28:12 +0530

    fixed the sql result naming

--- lib/server.php
+++ lib/server.php
@@ -102,8 +102,8 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$query = "SELECT * FROM users WHERE username='$username'";
-  	$results = mysqli_query($mysqli, $query);
-  	if (mysqli_num_rows($results) == 1) {
+  	$result = mysqli_query($mysqli, $query);
+  	if (mysqli_num_rows($result) == 1) {
 
       $row = mysqli_fetch_assoc($result);
       if(password_verify($password, $row["password"])){
