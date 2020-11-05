c34949428b50129be9bf130cb10474e36fce4228
thereelaman <amandwivedi@mail.com>
2020-11-05 21:05:01 +0530

    modified padding to the log out button

--- dashboard.php
+++ dashboard.php
@@ -41,7 +41,7 @@
 	<header class="header_area">
 		<div class="main_menu">
 			<nav class="navbar navbar-expand-lg navbar-light">
-				<div class="container" style="padding:1em 0em 0em 0em">
+				<div class="container" style="padding-top: 20px;">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<a class="navbar-brand logo_h" href="dashboard.php"><img src="img/logo.png" alt=""></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
