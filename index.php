db1c56f7f925609e6f0f48bf9976022ebf405b1e
thereelaman <amandwivedi@mail.com>
2020-11-07 02:33:44 +0530

    updated the titles and added the panelManager

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>LEDPanel Portal Login</title>
+	<title>LEDPanel Login Portal</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
--- panelManager.php
+++ panelManager.php
@@ -17,7 +17,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="icon"  href="/img/icons/favicon.ico" type="image/png"/>
-	<title>Dashboard</title>
+	<title>Panel Manager</title>
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="vendors/linericon/style.css">
@@ -46,9 +46,6 @@
 					<a class="navbar-brand logo_h" href="dashboard.php"><img src="img/logo.png" alt=""></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
 					</button>
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
@@ -57,7 +54,7 @@
 							<li class="nav-item"><a class="nav-link" href="/lib/logout.php" name="logout_user">Log Out</a></li>-->
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
-							<li class="nav-item" ><a href="/lib/logout.php" class="primary_btn text-uppercase">log out</a></li>
+							<li class="nav-item" ><a href="" class="primary_btn text-uppercase">upload</a></li>
 						</ul>
 					</div>
 				</div>
@@ -67,33 +64,14 @@
 	<section class="impress_area">
 		<div class="container">
 			<div class="impress_inner">
-				<h2>Welcome back, <?php echo($_SESSION['username'])?>!!!</h2>
-				<p>Thank you for being with us.</p>
-				<p><h2>The displays you own are:</h2></p>
-				<div class="container">
-              		<?php 
-				
-						$query = "SELECT panels.id, paneltype, token, temperature, brightness FROM panelOwner, panels WHERE panelOwner.user='".$_SESSION['username']."' and panels.id = panelOwner.id "; 
-                		$result = mysqli_query($mysqli, $query);
-				
-						echo "<table border='1' class='table table-hover'><tr><th  style='color:white;'>Panel ID</th><th style='color:white;'>Panel Type</th><th style='color:white;'>Token</th><th style='color:white;'>Temperature</th><th style='color:white;'>Brightness (%)</th></tr>";
-						while ($panel = mysqli_fetch_row($result)){
-							echo "<tr><td><a class=\"primary_btn\" href=\"/panelManager.php?id=$panel[0]\"><span>";
-							echo ($panel[0]);
-							echo "</span></a></td><td style='color:white;'>";
-							echo ($panel[1]);
-							echo "</td><td style='color:white;'>";
-							echo ($panel[2]);
-							echo "</td><td style='color:white;'>";
-							echo ($panel[3]);
-							echo "</td><td style='color:white;'>";
-							echo ($panel[4]);
-							echo "</td></tr>";
-						}
-						echo "</table>";
 
-              		?>
-				</div> 
+                <?php
+                    $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+                ?>
+
+				<h2>Hello, <?php echo($_SESSION['username'])?>!!!</h2>
+				<p>The display you are working on is:</p>
+				<p><h2>:</h2></p>
 			</div>
 		</div>
 	</section>
