e0786d4aff16edaa0eddb85fbb5ef03032604611
thereelaman <amandwivedi@mail.com>
2020-11-07 11:45:13 +0530

    added code to allow the panel to pass data to the database

--- api/panel/update.php
+++ api/panel/update.php
@@ -1,10 +1,5 @@
 <?php
     include('../../lib/db.php');
-    
-    // Check connection
-    if ($mysqli->connect_error) {
-        die("Database Connection failed: " . $conn->connect_error);
-    }
  
     if(!empty($_POST['id']) && !empty($_POST['token']) && !empty($_POST['temperature']) && !empty($_POST['brightness']))
     {
@@ -13,7 +8,7 @@
         $temperature = $_POST['temperature'];
         $brightness = $_POST['brightness'];
  
-	    $sql = "INSERT into rewarddata (dustbinid, temperature, rewardpoints) VALUES ('$id', '$temperature', $rewardmultiplier)";
+	    $sql = "UPDATE panels SET (temperature, brightness) VALUES ($temperature, $brightness) WHERE id = $id AND token = $token";
         
         if ($mysqli->query($sql) === TRUE) {
 		    echo "OK";
@@ -21,22 +16,8 @@
 		    echo "Error: " . $sql . "<br>" . $conn->error;
         }
         
-        $sql = "SELECT * FROM users WHERE temperature='$temperature'";
-        $result = $mysqli->query($sql);
-        $user   = mysqli_fetch_assoc($result);  
-
-        if($user['currentrewards'] <= $user['level']){
-
-            $reward = $user['rewardpoints'] + $rewardmultiplier;
-            $currreward = $user['currentrewards'] + $rewardmultiplier;  
-            
-
-
-            $sql = "UPDATE users SET rewardpoints='$reward', currentrewards='$currreward' ";
-        }
     }
     else{
         die("No POST Parameters passed.");
     }
-	$conn->close();
 ?>
